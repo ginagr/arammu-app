f79e9c85ab8a643253f6be656dfe5ec6caa9d49b
ggr <ginagonzalezroundey@gmail.com>
2020-04-10 10:31:44 -0400

    update packages and tslint

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const fs = window.require("fs");
+
 if (environment.production) {
   enableProdMode();
 }
